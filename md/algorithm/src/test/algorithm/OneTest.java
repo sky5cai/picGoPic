5af8279b7155749ece185f19dcd45b13a0d93271
sky5cai <1147861255@qq.com>
2020-02-12 12:46:30 +0800

    add algorithm

---
+++ md/algorithm/src/test/algorithm/OneTest.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import org.junit.Test;
+
+public class OneTest {
+
+    @Test
+    public void test1() throws Exception{
+        System.out.println("dddd");
+        System.out.println("dfdfdfd");
+    }
+}
